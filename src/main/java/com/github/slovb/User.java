12b4e8110ab7dea8278908839a64d6383ec286c6
Rickard Nyman <4slovb@gmail.com>
2025-01-28 12:15:14 +0100

    Javadoc added

--- src/main/java/com/github/slovb/User.java
+++ src/main/java/com/github/slovb/User.java
@@ -1,14 +1,48 @@
 package com.github.slovb;
 
+/**
+ * Represents a user.
+ *
+ * The user is uniquely identified by their email field.
+ */
 public class User {
+
+	/**
+	 * The users name.
+	 */
 	public String name;
+
+	/**
+	 * The users address as a single string.
+	 */
 	public String address;
+
+	/**
+	 * The users email.
+	 *
+	 * Uniquely identifies a user.
+	 */
 	public String email;
+
+	/**
+	 * The users telephone number in string form.
+	 */
 	public String telephone;
 	
+	/**
+	 * Default constructor. Required for JSON serialization.
+	 */
 	public User() {
 	}
 	
+	/**
+	 * Convenience constructor setting all the attributes.
+	 *
+	 * @param name
+	 * @param address
+	 * @param email
+	 * @param telephone
+	 */
 	public User(String name, String address, String email, String telephone) {
 		this.name = name;
 		this.address = address;
@@ -16,14 +50,25 @@ public class User {
 		this.telephone = telephone;
 	}
 	
+
+	/**
+	 * Returns a key that uniquely identifies the user.
+	 * Currently that is simply the email as given.
+	 *
+	 * @return	a string that uniquely identifies a user.
+	 */
 	public String getKey() {
 		// TODO: Consider UUID
-		// TODO: Motivate
 		return email;
 	}
 
+	/**
+	 * Does this user contain valid data?
+	 * Valid in the current context means all fields are not null or blank.
+	 *
+	 * @return	<code>true</code> if this user is valid.
+	 */
 	public boolean isValid() {
-		// TODO: Motivate
 		if (name == null || name.isBlank()) {
 			return false;
 		}
