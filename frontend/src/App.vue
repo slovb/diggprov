538d5252568dbb489eddaf79b27dfbd45fbe1253
Rickard Nyman <4slovb@gmail.com>
2025-01-30 13:15:57 +0100

    Mock user view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/user">User</RouterLink>
       </nav>
     </div>
   </header>
---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+class User {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+const users = ref([new User('Alice'), new User('Bob'), new User('Lotta')])
+const pageNumber = ref(0)
+const pageSize = 4
+const lastPage = computed(() => {
+  return Math.ceil(users.value.length / pageSize) - 1
+})
+
+const page = computed(() => {
+  const pos = pageSize * pageNumber.value
+  return users.value.slice(pos, pos + pageSize)
+})
+
+const newUserName = ref('')
+function addUser() {
+  users.value.push(new User(newUserName.value))
+  newUserName.value = ''
+}
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="user in page" :key="user.name">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+  <div>
+    <button v-if="pageNumber > 0" @click="pageNumber--">-</button>
+    <p>Page {{ pageNumber }}</p>
+    <button v-if="pageNumber < lastPage" @click="pageNumber++">+</button>
+  </div>
+  <div>
+    <input v-model="newUserName" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
